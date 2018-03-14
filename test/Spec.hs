19185af4de76f244dd21f45025ba3eda2b44ca99
Baojun Wang <wangbj@gmail.com>
2018-03-13 18:27:30 -0700

    add test case

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+import qualified Data.ByteString as S
+
+import           Data.DeviceTree
+import           Data.Either
+import           Test.Hspec
+import           Control.Monad.IO.Class
+
+junoDtbShouldParse = describe "able to parse juno.dtb built from Linux" $ do
+  it "parseDt juno.dtb" $ do
+    liftIO (S.readFile "juno.dtb") >>= \fdt -> isRight (parseDt fdt) `shouldBe` True
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  junoDtbShouldParse
