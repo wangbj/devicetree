39f7ccc3788ea80b3d001219c03c4f0a20725c79
Baojun Wang <wangbj@gmail.com>
2018-03-14 11:10:53 -0700

    more test cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,27 @@
+{-# LANGUAGE FlexibleContexts #-}
 import qualified Data.ByteString as S
-
+import           Data.ByteString (ByteString)
+import qualified Data.Map as Map
+import qualified Data.Text as Text
+import           Data.Map (Map)
+import           Data.Text (Text)
+import           Codec.Archive.Zip
 import           Data.DeviceTree
 import           Data.Either
 import           Test.Hspec
-import           Control.Monad.IO.Class
+import           Control.Monad
 
-junoDtbShouldParse = describe "able to parse juno.dtb built from Linux" $ do
-  it "parseDt juno.dtb" $ do
-    liftIO (S.readFile "juno.dtb") >>= \fdt -> isRight (parseDt fdt) `shouldBe` True
+extractAll :: [EntrySelector] -> ZipArchive [(String, ByteString)]
+extractAll = mapM extract
+  where
+    extract :: EntrySelector -> ZipArchive (String, ByteString)
+    extract e = getEntry e >>= \s -> return (Text.unpack (getEntryName e), s)
+
+canParseAllDtbs dtbs = describe "parse all dtbs for Linux/arm64" $ do
+  entries <- runIO $ withArchive dtbs (extractAll =<< (Map.keys <$> getEntries))
+  it "dtbs is not empty" (null entries `shouldBe` False)
+  mapM_ (\(i, s) -> it ("parse " ++ i) ( isRight (parseDt s) `shouldBe` True)) entries
 
 main :: IO ()
 main = hspec $ do
-  junoDtbShouldParse
+  canParseAllDtbs "test/dtbs.zip"
